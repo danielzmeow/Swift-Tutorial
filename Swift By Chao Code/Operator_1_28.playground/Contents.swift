54c4016cae08d89fa26e9d91bf75f69a95412e5c
Daniel Zhang <Daniel20482048@outlook.com>
2025-04-08 21:17:45 +0800

    First Commit

---
+++ Swift By Chao Code/Operator_1_28.playground/Contents.swift
@@ -0,0 +1,20 @@
+//Remainder
+//To identify full devision
+var number = 9 % 2
+print(number)
+
+//=
+var a = 1 + 2
+//add then output
+a += 2
+//remainder then output
+a %= 3
+
+//Use variable
+var number2 = 5
+number2 += number2
+number2 -= 3
+
+//Only interger part output
+var test = 3
+100 / test
